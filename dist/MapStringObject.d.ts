5b65e14eefbce8dfd1936e3726bafc3a38074660
JAM Types Builder <devops@jamplus.com>
2025-07-15 20:39:37 +0000

    Schema type update

--- dist/MapStringObject.d.ts
+++
@@ -1,4 +0,0 @@
-import { z } from "zod/v4";
-export declare const MapStringObject: z.ZodRecord<z.ZodString, z.ZodAny>;
-export type MapStringObject = z.infer<typeof MapStringObject>;
-//# sourceMappingURL=MapStringObject.d.ts.map
